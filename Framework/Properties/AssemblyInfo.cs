4e278eb874d908c34d121775962a56ac8b73b3e4
John Wang <john29917958@gmail.com>
2017-07-31 11:18:22 +0800

    Updates assembly information.

--- Framework/Properties/AssemblyInfo.cs
+++ Framework/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("KAutomation")]
+[assembly: AssemblyDescription("A Korat framework with expressive, elegant syntax.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Framework")]
+[assembly: AssemblyCompany("Object-Oriented Lab @ National Central University, CSIE department, Taiwan (R.O.C.).")]
+[assembly: AssemblyProduct("KAutomation")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
+
