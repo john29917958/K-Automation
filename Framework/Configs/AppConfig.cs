e2cd915add43523536d55345902592ebdf025490
John Wang <john29917958@gmail.com>
2017-07-31 18:40:31 +0800

    Add Config

---
+++ Framework/Configs/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Ncu.Oolab.Korat.KAutomation.Configs
+{
+    public enum Environments { Develop, Testing, Production }
+
+    public class AppConfig
+    {
+        public string Name { get; set; }
+        public Environments Environment { get; set; }
+        public string Author { get; set; }
+        public string Manager { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ Framework/Configs/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Ncu.Oolab.Korat.KAutomation.Configs
+{
+    public class Configuration
+    {
+        public AppConfig App { get; protected set; }
+    }
+}
